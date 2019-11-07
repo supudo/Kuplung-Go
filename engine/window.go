5f0d35763350364d699c078aa373a1577c97f162
Sergey Petrov <supudo@gmail.com>
2019-11-07 15:43:26 +0200

    rev

--- engine/window.go
+++ engine/window.go
@@ -1,6 +1,7 @@
 package engine
 
 import (
+	"github.com/go-gl/gl/v2.1/gl"
 	"github.com/supudo/Kuplung-Go/settings"
 	"github.com/veandco/go-sdl2/sdl"
 )
@@ -39,5 +40,30 @@ func initSDL() *sdl.Window {
 		settings.LogError("[initSDL] Failed to create window: %v", err)
 	}
 
+	setupOpenGL(window)
+
 	return window
+}
+
+func setupOpenGL(window *sdl.Window) {
+	err := gl.Init()
+	if err != nil {
+		settings.LogError("[setupOpenGL] Failed to initialize OpenGL: %v", err)
+	}
+
+	glContext, err := window.GLCreateContext()
+	if err != nil {
+		settings.LogError("[setupOpenGL] Failed to create OpenGL context: %v", err)
+	}
+	defer sdl.GLDeleteContext(glContext)
+
+	err = window.GLMakeCurrent(glContext)
+	if err != nil {
+		settings.LogError("[setupOpenGL] Failed to set current OpenGL context: %v", err)
+	}
+
+	err = sdl.GLSetSwapInterval(1)
+	if err != nil {
+		settings.LogError("[setupOpenGL] Failed to set swap interval: %v", err)
+	}
 }
