4ea21ad3513042e60db127247a6b4b2108e29f46
Sergey Petrov <supudo@gmail.com>
2019-12-02 09:52:34 +0200

    rev

--- settings/logger.go
+++ settings/logger.go
@@ -10,8 +10,8 @@ import (
 // LogError logs an error and exits the application
 func LogError(format string, args ...interface{}) {
 	msg := fmt.Sprintf(format, args...)
-	pc, fn, line, _ := runtime.Caller(1)
-	log.Fatalf("[%s:%d] / [Kuplung] %v", fn, line, msg)
+	_, fn, line, _ := runtime.Caller(1)
+	log.Fatalf("[Kuplung] %s:%d - %v", fn, line, msg)
 	os.Exit(3)
 }
 
