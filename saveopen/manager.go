aa00ed628e4f87101cbfb1da31a358c2627e2eef
Sergey Petrov <supudo@gmail.com>
2020-01-14 14:28:46 +0200

    rev

--- saveopen/manager.go
+++ saveopen/manager.go
@@ -5,31 +5,31 @@ import (
 	"github.com/supudo/Kuplung-Go/types"
 )
 
-// SaveOpenManager ...
-type SaveOpenManager struct {
+// SOManager ...
+type SOManager struct {
 	soProtobufs *ProtoBufsSaveOpen
 
 	doProgress func(float32)
 }
 
 // NewSaveOpenManager ...
-func NewSaveOpenManager(doProgress func(float32)) *SaveOpenManager {
-	som := &SaveOpenManager{}
+func NewSaveOpenManager(doProgress func(float32)) *SOManager {
+	som := &SOManager{}
 	som.doProgress = doProgress
 	som.initProtobufs()
 	return som
 }
 
 // Save ...
-func (som *SaveOpenManager) Save(file *types.FBEntity, meshes []*meshes.ModelFace) {
+func (som *SOManager) Save(file *types.FBEntity, meshes []*meshes.ModelFace) {
 	som.soProtobufs.Save(file, meshes)
 }
 
 // Open ...
-func (som *SaveOpenManager) Open(file *types.FBEntity) []*meshes.ModelFace {
+func (som *SOManager) Open(file *types.FBEntity) []*meshes.ModelFace {
 	return som.soProtobufs.Open(file)
 }
 
-func (som *SaveOpenManager) initProtobufs() {
+func (som *SOManager) initProtobufs() {
 	som.soProtobufs = NewProtoBufsSaveOpen(som.doProgress)
 }
